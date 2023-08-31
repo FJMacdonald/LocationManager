b3634d98c22d7e40d3c837c75a12ad9c9e9170c8
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-31 09:24:03 -0400

    lesson 8.8: Adding place lookup using locationManager.  Uses MKPlacemark/CLPlacemark to build an address from place mark components.

--- LocationManager/ContentView.swift
+++ LocationManager/ContentView.swift
@@ -6,22 +6,40 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     @EnvironmentObject var locationManager: LocationManager
-    
+    @State private var showPlaceLookupSheet = false
+    @State var returnedPlace = Place(mapItem: MKMapItem())
     var body: some View {
         VStack (alignment: .leading) {
             
             Text("Locatiion:\n\(locationManager.location?.coordinate.latitude ?? 0.0), \(locationManager.location?.coordinate.longitude ?? 0.0)")
+            Text("\nReturned Place: \nName: \(returnedPlace.name)\nAddress:\(returnedPlace.address)\nCoords: \(returnedPlace.latitude), \(returnedPlace.longitude)")
         }
         .padding()
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                Button {
+                    showPlaceLookupSheet.toggle()
+                } label: {
+                    Image(systemName: "magnifyingglass")
+                    Text("Lookup Place")
+                }
+            }
+        }
+        .fullScreenCover(isPresented: $showPlaceLookupSheet) {
+            PlaceLookupView(returnedPlace: $returnedPlace)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .environmentObject(LocationManager())
+        NavigationStack {
+            ContentView()
+                .environmentObject(LocationManager())
+        }
     }
 }
