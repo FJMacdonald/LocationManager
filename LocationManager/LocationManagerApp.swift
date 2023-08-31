8c13d7f46e8e3c1204df7d2e29bbd706835cb055
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-31 03:44:24 -0400

    Showing the steps required to get location working in an app

--- LocationManager/LocationManagerApp.swift
+++ LocationManager/LocationManagerApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct LocationManagerApp: App {
+    @StateObject var locationManager = LocationManager()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(locationManager)
         }
     }
 }
