12558d27c7a354d026fdbaba39c332e716871d61
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-31 03:13:58 -0400

    Initial Commit

---
+++ LocationManager/LocationManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationManagerApp.swift
+//  LocationManager
+//
+//  Created by Francesca MACDONALD on 2023-08-31.
+//
+
+import SwiftUI
+
+@main
+struct LocationManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
